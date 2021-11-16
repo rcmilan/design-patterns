bf481fa39cc851fe8356bcccdeb510788f0bc27b
Ricardo Milan <ricardo@intercambiodireto.com>
2021-11-16 09:03:06 -0300

    Singleton

---
+++ Design-Patterns/Controllers/SingletonController.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Singleton;
+
+namespace DesignPatterns.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SingletonController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Car> Get()
+        {
+            // busca a instancia dentro do singleton
+            // todas as chamadas a GetInstance retornarão o mesmo objeto em memória
+            var result = CarSingleton.GetInstance();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Singleton/CarSingleton.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Models;
+
+namespace Singleton
+{
+    public class CarSingleton
+    {
+        // propriedade static para que a mesma instancia esteja disponivel para todo o sistema
+        private static Car _car;
+
+        // construtor é private para evitar que a classe seja instanciada
+        private CarSingleton()
+        {
+
+        }
+
+        /// <summary>
+        /// Cria ou retorna a instancia do singleton
+        /// </summary>
+        /// <returns>Car</returns>
+        public static Car GetInstance()
+        {
+            if (_car == null)
+                _car = new Car { Model = "Singleton", Color = System.Drawing.Color.Black, Type = "Singleton" };
+
+            return _car;
+        }
+    }
+}
