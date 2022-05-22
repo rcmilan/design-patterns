ba4ad03e6c51ad9ee30b4ad5af28810e4a8df7ff
Ricardo Milan <ricardo@fluencypass.com>
2022-05-22 10:30:09 -0300

    Monoid

---
+++ App/Controllers/MonoidController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Monoid.Structs;
+
+namespace DesignPatterns.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MonoidController : ControllerBase
+    {
+        private static MFunction<int> Add5 => new(a => a + 5);
+        private static MFunction<int> Add10 => new(a => a + 10);
+
+        [HttpGet]
+        public int AddInteger(int number)
+        {
+            var add15 = Add5 + Add10;
+
+            return add15.Run(number);
+        }
+    }
+}
