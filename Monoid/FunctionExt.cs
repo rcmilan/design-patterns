7f9fea89e03fccb530ebce12ac93a5e68a5dd429
Ricardo Milan <ricardo@fluencypass.com>
2022-05-22 11:11:24 -0300

    Monoid

--- Monoid/FunctionExt.cs
+++
@@ -1,9 +0,0 @@
-﻿using Monoid.Structs;
-
-namespace Monoid
-{
-    public static class FunctionExt
-    {
-        public static MFunction<T> Then<T>(this MFunction<T> thisM, MFunction<T> next) => new(x => thisM.Run(next.Run(x)));
-    }
-}
